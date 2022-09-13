34efb4385c05e109eb29db833bdb66e1512c5310
kaiba <kaibadash@gmail.com>
2022-09-13 23:51:18 +0900

    Add sample static site

---
+++ main.js
@@ -0,0 +1,29 @@
+document.getElementById("execute").onclick = () => {
+  let dirHandle = window.showDirectoryPicker();
+  console.log(dirHandle);
+
+  dirHandle.then(function (handle) {
+    console.log(handle);
+
+    handle
+      .getDirectoryHandle(new Date().getTime(), {
+        create: true,
+      })
+      .then(function (newDirectoryHandle) {
+        console.log(newDirectoryHandle);
+
+        newDirectoryHandle
+          .getFileHandle("test.txt", { create: true })
+          .then(function (sampleHandle) {
+            console.log("test:", sampleHandle);
+
+            sampleHandle.createWritable().then(function (writable) {
+              writable.write("handle! It's " + new Date()).then(function (a) {
+                console.log(a);
+                writable.close();
+              });
+            });
+          });
+      });
+  });
+};
